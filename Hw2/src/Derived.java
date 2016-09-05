aaac3b6afc945c1ca84f8e22f95ad1a286bcc3a3
Asheem Chhetri <Asheem Chhetri>
2016-09-05 01:54:20 -0400

    completed HW1 and HW2

---
+++ Hw2/src/Derived.java
@@ -0,0 +1,11 @@
+public class Derived extends Base {
+
+   public Derived( ){   } //Constructor
+   
+   //add necessary function(s) here.
+   //Base class overriding
+   public void f2()
+   {
+	   System.out.println("Derived f2");
+   }
+}

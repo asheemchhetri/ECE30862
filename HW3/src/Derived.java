83ee30a187b0a982d74c3b5ec9d84489e126e467
Asheem Chhetri <Asheem Chhetri>
2016-09-08 22:01:05 -0400

    Correct file added

---
+++ HW3/src/Derived.java
@@ -0,0 +1,11 @@
+public class Derived extends Base {
+
+   public Derived( ) { }
+
+  // Add any needed function(s) here.
+   public void f1(double num)
+   {
+	   System.out.println("Deriverd f1(float)");
+   }
+}
+

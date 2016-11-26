af6cff2a8a2173ea8b2bcd1e76cd0ce0a67559f6
Asheem Chhetri <Asheem Chhetri>
2016-11-26 00:02:53 -0500

    50% of Java Web App Project done

---
+++ JavaProject/eMusicStore/src/main/java/com/emusicstore/dao/ProductDao.java
@@ -0,0 +1,19 @@
+package com.emusicstore.dao;
+
+import com.emusicstore.model.Product;
+
+import java.util.List;
+
+/**
+ * Created by Asheem on 11/25/2016.
+ */
+public interface ProductDao {
+
+	void addProduct(Product product);
+
+	Product getProductById(String id);
+
+	List<Product> getAllProducts();
+
+	void deleteProduct(String id);
+}
